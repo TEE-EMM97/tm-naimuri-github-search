12c81761f88dc6a89789ac632fc0a980e155b8be
TEE-EMM97 <tevmc@live.com>
2022-06-26 23:17:49 +0100

    doc: miscellaneous files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react';
-import Intro from '../components/intro';
-import Search from '../components/search';
+import Intro from '../components/Intro';
+import Search from '../components/Search';
+import SearchResults from '../components/SearchResults';
 
 const Home: NextPage = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <Intro />
       <Search setSearchTerm={setSearchTerm} />
+      <SearchResults searchTerm={searchTerm}/>
     </>
   );
 }
