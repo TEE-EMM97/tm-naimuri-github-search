a664eb0185f50e5a0d1175ede612608eea91eb7d
TEE-EMM97 <tevmc@live.com>
2022-06-26 20:50:02 +0100

    initial commit
    Removed boilerplate code and moved pages and styles into `src` folder with the addition of a lib and components folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Naimuri - Github Search</title>
+        <meta name="description" content="A search engine for github repositories" />
+         <meta name="author" content="Tevon McCrea"></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
+
+export default Home
