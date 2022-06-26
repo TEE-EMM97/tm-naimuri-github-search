a0c8351549f9a55593cd2e9567ddda4770ed1c4a
TEE-EMM97 <tevmc@live.com>
2022-06-27 00:10:41 +0100

    style: styling for repository details page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 import { useState } from 'react';
 import Intro from '../components/Intro';
 import Search from '../components/Search';
@@ -11,18 +11,12 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Naimuri - Github Search</title>
-        <meta
-          name="description"
-          content="A search engine for github repositories"
-        />
-        <meta name="author" content="Tevon McCrea"></meta>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Intro />
       <Search setSearchTerm={setSearchTerm} />
-      <SearchResults searchTerm={searchTerm}/>
+      <SearchResults searchTerm={searchTerm} />
     </>
   );
-}
+};
 
-export default Home
+export default Home;
